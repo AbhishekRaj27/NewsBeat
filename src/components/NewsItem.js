5a1e8b47cb03fef2df6e08a64e7a62bf02bb1a2f
Abhishek Raj <abhishek.raj.singh27@gmail.com>
2023-07-08 04:47:42 +0530

    populate NewsItems

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,12 +4,20 @@ export class NewsItem extends Component {
   render() {
     let { title, description, imageUrl, newsUrl } = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://i.ytimg.com/vi/tEnoFinTrIY/maxres2.jpg?sqp=-oaymwEoCIAKENAF8quKqQMcGADwAQH4AbYIgAKAD4oCDAgAEAEYWSBhKGUwDw==&rs=AOn4CLDa1jeHlvhnQzFlj9aXoJY9O2dFew"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
             <a
               href={newsUrl}
               target="_blank"
